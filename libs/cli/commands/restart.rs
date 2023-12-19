fd88a0ab349e9db024c58912aede95f8777979e7
Emilien Jegou <emj339@gmail.com>
2023-12-19 04:21:41 +0100

    feat(cli): restart command

--- libs/cli/commands/restart.rs
+++ libs/cli/commands/restart.rs
@@ -1,10 +1,22 @@
 use clap::Args;
-use o324_core::Core;
+use o324_core::{Core, StartTaskInput};
 
 #[derive(Args, Debug)]
 pub struct Command {}
 
-pub async fn handle(_: Command, _core: &Core) -> eyre::Result<()> {
-    unimplemented!();
-}
+pub async fn handle(_: Command, core: &Core) -> eyre::Result<()> {
+    let task = core
+        .list_last_tasks(1)
+        .await?
+        .pop()
+        .ok_or_else(|| eyre::eyre!("No task to restart"))?;
 
+    core.start_new_task(StartTaskInput {
+        task_name: task.task_name,
+        project: task.project,
+        tags: task.tags,
+    })
+    .await?;
+
+    Ok(())
+}
