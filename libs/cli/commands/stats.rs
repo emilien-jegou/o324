ff93c8b97fa6a5ac6169ca206dede60e9d24a4c1
emilien-jegou <88588240+emilien-jegou@users.noreply.github.com>
2024-02-22 21:27:57 +0100

    feat(CLI): improve log messages formatting
    ** What is the motivation for the change?
    
    I want a better way to visualize tasks in the CLI.
    
    ** How does it address the problem?
    
    We now have three output options:
    
    A short version that we get by default that omit a lot of informations
    tasks (omit ID, task end date ...).
    
    A detailed version when running with `--verbose` that display
    informations in a table format.
    
    A json version when running with `--json` that display the tasks as a
    json array.

--- libs/cli/commands/stats.rs
+++ libs/cli/commands/stats.rs
@@ -2,7 +2,11 @@ use clap::Args;
 use o324_core::Core;
 
 #[derive(Args, Debug)]
-pub struct Command {}
+pub struct Command {
+    /// Name of the project
+    #[clap(short, long)]
+    verbose: bool,
+}
 
 pub async fn handle(_: Command, _core: &Core) -> eyre::Result<()> {
     unimplemented!();
