2f36472a2230504cbe51eda7abe6898dfc8c9a2f
Emilien Jegou <emj339@gmail.com>
2024-05-19 04:37:17 +0200

    feat(GUI): method helper function
    This commit permit the invocation methods from the frontend the dev
    console through the ‘window’ JS global variable.

--- libs/gui/src/api/methods.ts
+++ libs/gui/src/api/methods.ts
@@ -1,27 +1,32 @@
 import { invoke } from '@tauri-apps/api/core';
 import { Task } from './definitions';
 
-export const listLastTasks = (count: number): Promise<Task[]> => invoke('list_last_tasks', { count });
+const method = function <Ret, T extends unknown[]>(name: string, m?: (...args: T) => Record<string, unknown>): (...args: T) => Promise<Ret> {
+  (window as any).__METHOD = { ...((window as any).__METHOD ?? {}), [name]: m };
+  return (...args: T) => invoke(name, m?.(...args) ?? {});
+}
 
-export const stopCurrentTask = () => invoke('stop_current_task', {});
+export const listLastTasks = method<Task[], [number]>('list_last_tasks', (count: number) => ({ count }));
 
-export const deleteTaskByUlid = (task_id: string) => invoke('delete_task_by_ulid', { ulid: task_id });
+export const stopCurrentTask = method('stop_current_task');
 
-export const synchronizeTasks = () => invoke('synchronize_tasks', {});
+export const deleteTaskByUlid = method('delete_task_by_ulid', (task_id: string) => ({ ulid: task_id }));
 
-export const getCurrentConfig = () => invoke('get_current_config', {});
+export const synchronizeTasks = method('synchronize_tasks');
+
+export const getCurrentConfig = method('get_current_config');
 
 // TODO: can we set remove the project value using this method?
 type EditTaskUpdateInput = { task_name: string | null, project: string | null, tags: string[] | null };
-export const editTask = (task_id: string, task_update: EditTaskUpdateInput) =>
-  invoke('edit_task', { ulid: task_id, data: task_update });
+
+export const editTask = method('edit_task', (task_id: string, task_update: EditTaskUpdateInput) => ({ ulid: task_id, data: task_update }));
 
 type StartNewTaskInput = { task_name: string, project: string | null, tags: string[] };
-export const startNewTask = (task: StartNewTaskInput) => invoke('start_new_task', { data: task });
+export const startNewTask = method('start_new_task', (task: StartNewTaskInput) => ({ data: task }));
 
 type SaveNewConfigInput = Record<string, any>;
-export const saveNewConfig = (config: SaveNewConfigInput) => invoke('save_new_config', { config: config });
+export const saveNewConfig = method('save_new_config', (config: SaveNewConfigInput) => ({ config: config }));
 
-export const loadProfile = (profile: string) => invoke('load_profile', { profile });
+export const loadProfile = method('load_profile', (profile: string) => ({ profile }));
 
-export const updateTrayIcon = (active: boolean) => invoke('update_tray_icon', { active });
+export const updateTrayIcon = method('update_tray_icon', (active: boolean) => ({ active }));
